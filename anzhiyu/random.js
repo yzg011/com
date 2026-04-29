78ca76103e4c1ffdf45cb6718ba9e575dad273ac
yzg011 <357239268@qq.com>
2026-04-29 15:37:34 +0800

    Site updated: 2026-04-29 15:37:24

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/04/27/hello-world/","2026/04/29/“宋朝”/","2026/04/27/张居正：一根燃烧自己的脊梁/","2026/04/15/李清照/","2026/04/27/苏轼与苏辙：千年兄弟情的传奇/","2026/04/27/陆游与唐婉的爱情故事/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
