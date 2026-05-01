a3e4f7298f1de41cc1f668d476e1f009f542e5b3
yzg011 <357239268@qq.com>
2026-05-01 12:52:43 +0800

    Site updated: 2026-05-01 12:52:43

---
+++ js/baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cc22fd91218fb6aa981122d2f6dcb1e4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
